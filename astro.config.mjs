4f572ca1588f44611bc9d54fae6013dab2bf448b
nareshv-dev <nareshvenkadesh@gmail.com>
2026-02-12 12:18:05 +0530

    Fix: Remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://nareshv-dev.github.io',
-  base: '/naresh-portfolio/',
+  site: 'https://naresh-v.dev',
   integrations: [tailwind()],
   output: 'static',
 });
