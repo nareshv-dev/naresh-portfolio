ee4d95ce513e64c7b71fe611be3d4263bb4c29b1
nareshv-dev <nareshvenkadesh@gmail.com>
2026-02-12 10:35:25 +0530

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://nareshv-dev.github.io',
+  base: '/naresh-portfolio',
   integrations: [tailwind()],
   output: 'static',
   devToolbar: {
