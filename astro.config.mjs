3d27c171c1fdd9e6de214cffcfbdf366e217b081
nareshv-dev <nareshvenkadesh@gmail.com>
2026-02-12 11:47:04 +0530

    Add profile photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://nareshv-dev.github.io',
-  base: '/naresh-portfolio',
-  integrations: [tailwind()],
-  output: 'static',
-  devToolbar: {
-    enabled: false
-  }
+  site: 'https://naresh-v.dev'
 });
